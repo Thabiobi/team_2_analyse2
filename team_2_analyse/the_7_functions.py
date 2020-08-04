49b819c0c4e50c4ad9e6488f034770f17ac096cc
Thabiobi <thabisileobi@gmail.com>
2020-08-04 12:03:59 +0200

    Added mun_dict variable

--- team_2_analyse/the_7_functions.py
+++ team_2_analyse/the_7_functions.py
@@ -1,7 +1,23 @@
+mun_dict = {
+    '@CityofCTAlerts' : 'Cape Town',
+    '@CityPowerJhb' : 'Johannesburg',
+    '@eThekwiniM' : 'eThekwini' ,
+    '@EMMInfo' : 'Ekurhuleni',
+    '@centlecutility' : 'Mangaung',
+    '@NMBmunicipality' : 'Nelson Mandela Bay',
+    '@CityTshwane' : 'Tshwane'
+}
+
 # function 1
 # function 2
 # function 3
 # function 4
+def extract_municipality_hashtags(df):
+    """
+    doc string
+    """
+
+
 # function 5
 # function 6
 # function 7
